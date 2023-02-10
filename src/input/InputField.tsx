ca2dd8e806975a6f9ef0389d018f4fb31c48876c
Badlylucky <u770518g@ecs.osaka-u.ac.jp>
2023-02-10 17:41:14 +0900

    表示順の変更

--- src/input/InputField.tsx
+++ src/input/InputField.tsx
@@ -1,10 +1,10 @@
 import React, { ChangeEvent, useState } from 'react';
-import {Switch, Input, InputNumber, Space, Row, Col, Upload, UploadProps, UploadFile} from 'antd';
+import assert from 'assert';
+import {Switch, Input, InputNumber, Row, Col, Space, Upload, UploadProps, UploadFile} from 'antd';
 import {CheckOutlined} from '@ant-design/icons';
 import {UploadRequestOption as RcCustomRequestOptions} from 'rc-upload/lib/interface';
 import {Colorpicker, AnyColorFormat} from 'antd-colorpicker';
 import 'antd/dist/reset.css';
-import assert from 'assert';
 
 interface Status {
   level: number;
@@ -81,60 +81,66 @@ const InputField = (props: Status) => {
     setFileList(newFileList);
   };
   return (
-    <Row gutter={[0, 2]}>
-      <Col span={12}>
-        <Input style={{width: 'calc(100% - 24px)'}} 
-          placeholder="MASTER"
-          addonBefore="Difficulty"
-          onChange={changeDifficulty}>
-        </Input>
-      </Col>
-      <Col span={12}>
-        <InputNumber style={{width: 'calc(75% - 24px)'}} 
-          defaultValue={nowStatus.level} placeholder="14" min={0} max={99}
-          addonBefore="Level"
-          addonAfter={<Switch checkedChildren={<CheckOutlined/>} onChange={changeLevelVisible}/>} 
-          onChange={changeLevel}>
-        </InputNumber>
-      </Col>
-      <Col span={24}>
-        <Colorpicker defaultValue={nowStatus.color}
-          popout
-          value={nowcolor}
-          onChange={changeColor}
-          presetColors={['#00ab84', '#ff7e00', '#f12929', '#8e1be5', '#16ff47', '#ffba00', '#fa0667', '#a810ff']}>
-        </Colorpicker>
-      </Col>
-      <Col span={24}>
-        <Input style={{width: 'calc(100% - 24px)'}}
-          placeholder="其のエメラルドを見よ"
-          addonBefore="Title"
-          onChange={changeTitle}>
-        </Input>
-      </Col>
-      <Col span={24}>
-        <Input style={{width: 'calc(100% - 24px)'}}
-          placeholder="庭師"
-          addonBefore="Composer"
-          onChange={changeComposer}>
-        </Input>
-      </Col>
-      <Col span={24}>
-        {/* <label>Cover Image: </label>
-        <input type="file" name="cover" accept="image/png, image/jpeg, image/gif"
-          onChange={changeCoverChange}>
-        </input> */}
-        <Upload
-          accept='image/*'
-          listType='picture-card'
-          fileList={fileList}
-          onChange={changeCoverChange}
-          customRequest={customRequest}
-        >
-          {fileList.length < 1 && '+ Upload'}
-        </Upload>
-      </Col>
-    </Row>
+    <Space direction='vertical' style={{width: '100%'}}>
+      <Row>
+        <Col span={24}>
+          <Input style={{width: 'calc(75% - 24px)'}} 
+            placeholder="MASTER"
+            addonBefore="Difficulty"
+            onChange={changeDifficulty}>
+          </Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <InputNumber style={{width: '75%'}} 
+            defaultValue={nowStatus.level} placeholder="14" min={0} max={99}
+            addonBefore="Level"
+            addonAfter={<Switch checkedChildren={<CheckOutlined/>} onChange={changeLevelVisible}/>} 
+            onChange={changeLevel}>
+          </InputNumber>
+        </Col>
+      </Row>
+      <Row align={'middle'}>
+        <Col span={12}>
+          <Colorpicker defaultValue={nowStatus.color}
+            popout
+            value={nowcolor}
+            onChange={changeColor}
+            presetColors={['#00ab84', '#ff7e00', '#f12929', '#8e1be5', '#16ff47', '#ffba00', '#fa0667', '#a810ff']}>
+          </Colorpicker>
+        </Col>
+        <Col span={12} flex={2}>
+          <Upload
+            accept='image/*'
+            listType='picture-card'
+            fileList={fileList}
+            onChange={changeCoverChange}
+            customRequest={customRequest}
+          >
+            {fileList.length < 1 && '+ Upload'}
+          </Upload>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <Input style={{width: 'calc(100% - 24px)'}}
+            placeholder="其のエメラルドを見よ"
+            addonBefore="Title"
+            onChange={changeTitle}>
+          </Input>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <Input style={{width: 'calc(100% - 24px)'}}
+            placeholder="庭師"
+            addonBefore="Composer"
+            onChange={changeComposer}>
+          </Input>
+        </Col>
+      </Row>
+    </Space>
   );
 }
 
